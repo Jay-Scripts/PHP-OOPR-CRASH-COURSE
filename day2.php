7b7a22540ec9a588f6de9fb76fa3b064ed24389a
JayScriptt <corneliogatbontonjr21@gmail.com>
2025-08-01 02:44:21 +0800

    PUBLIC ACCESS MODIFIER

--- day2.php
+++ day2.php
@@ -13,9 +13,9 @@
     class Product
     {
         // objects
-        private $productName;
-        private $price;
-        private $stockQuantity;
+        public $productName;
+        public $price;
+        public $stockQuantity;
 
         // constructor
         public function __construct($productName, $price, $stockQuantity)
@@ -28,8 +28,13 @@
     // instance
     $product1 = new Product("Milo <br>", "12 <br>", "50 <br>");
     $product2 = new Product("Ovaltine <br>", "10 <br>", "100 <br>");
-    print_r($product1);
-    print_r($product2)
+    // print_r($product1);
+    // print_r($product2)
+
+    // ACCESSING THE PROPERTIES IT WAS SET AS PUBLIC
+    echo " Product  : $product1->productName <br> 
+           Price    : $product1->price <br>
+           Quantity : $product1->stockQuantity";
     ?>
 
 </body>
